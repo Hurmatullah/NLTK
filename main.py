8f87469f04b4d9b1591233b0eebc44d80ae54625
= <hrasaa1@gmail.com>
2022-11-26 12:57:04 -0800

    Showing the distinct terms in vocabulary that we have chosen

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from nltk import book
 # Printing the lenght of the vocabulary
 # print("Length of vocabulary", len(book.text1))
 
-# Sorting our text in NLTK using python
-print("Sorting the text", sorted(set(book.text1)))
+# # Sorting our text in NLTK using python
+# print("Sorting the text", sorted(set(book.text1)))
+
+# Showing the how much distinct terms we have
+# print("Distinct terms in our vocabulary", len(set(book.text1)) / len(book.text1))
