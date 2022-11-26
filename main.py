37a0bbc7bb580950f22f01d6bfed85c572fab186
= <hrasaa1@gmail.com>
2022-11-26 12:51:38 -0800

    Sorting our terms in a vocabulary that called text1

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from nltk import book
 
 # Printing the lenght of the vocabulary
-print("Length of vocabulary", len(book.text1))
+# print("Length of vocabulary", len(book.text1))
+
+# Sorting our text in NLTK using python
+print("Sorting the text", sorted(set(book.text1)))
